86617002b3c5f36d7f4af85b486f6bc8371e1996
magic8baller <lturis@gmail.com>
2019-02-18 17:29:48 -0500

    extract table

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,8 +1,7 @@
 import React, { Component } from 'react'
-import TableHeader from './common/tableHeader'
+import Table from './common/table'
 import Like from './common/like'
 
-import TableBody from './common/tableBody'
 class MoviesTable extends Component {
   // add new property set as jsx expression ! is really jus a good, POJO!
   columns = [
@@ -32,14 +31,12 @@ class MoviesTable extends Component {
   render() {
     const { movies, onSort, sortColumn } = this.props
     return (
-      <table className="table">
-        <TableHeader
-          columns={this.columns}
-          sortColumn={sortColumn}
-          onSort={onSort}
-        />
-        <TableBody data={movies} columns={this.columns} />
-      </table>
+      <Table
+        columns={this.columns}
+        data={movies}
+        sortColumn={sortColumn}
+        onSort={onSort}
+      />
     )
   }
 }
