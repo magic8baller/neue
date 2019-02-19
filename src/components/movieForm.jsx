a9e30ce4804abc7fda837f3f613f4e09b2089ba2
magic8baller <lturis@gmail.com>
2019-02-19 13:13:31 -0500

    add movieform routing, dynamic programming

--- src/components/movieForm.jsx
+++ src/components/movieForm.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
-
-const MovieForm = () => {
+//return user to movies page on click
+const MovieForm = ({ match, history }) => {
   return (
     <div>
-      <h1>Movie Form</h1>
-      <button className="btn btn-primary">Save</button>
+      <h1>Movie Form - {match.params.id}</h1>
+
+      <button
+        className="btn btn-primary"
+        onClick={() => history.push('/movies')}
+      >
+        Save
+      </button>
     </div>
   )
 }
--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,14 +1,19 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import Table from './common/table'
 import Like from './common/like'
 
 class MoviesTable extends Component {
   columns = [
-    { path: 'title', label: 'Title' },
+    {
+      path: 'title',
+      label: 'Title',
+      content: movie => <Link to={`/movies/${movie._id}`}>{movie.title}</Link>
+    },
     { path: 'genre.name', label: 'Genre' },
     { path: 'numberInStock', label: 'Stock' },
     { path: 'dailyRentalRate', label: 'Rate' },
-    // set jsx objects as fn() with 'movie' as parameter
+
     {
       key: 'like',
       content: movie => (
