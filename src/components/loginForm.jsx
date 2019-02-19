8f28af9ad83f9f1ce8d89ae3638b474b38f11d7f
magic8baller <lturis@gmail.com>
2019-02-19 14:55:53 -0500

    extract userinput

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,19 +1,44 @@
 import React, { Component } from 'react'
-
+import Input from './common/input'
 class LoginForm extends Component {
+  state = {
+    account: {
+      username: '',
+      password: ''
+    }
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+
+    //callserver ....
+    console.log('submitted')
+  }
+
+  handleChange = ({ currentTarget: input }) => {
+    const account = { ...this.state.account }
+    account[input.name] = input.value
+    this.setState({ account })
+  }
+
   render() {
+    const { username, password } = this.state.account
     return (
       <div className="container">
         <h1>Login</h1>
         <form onSubmit={this.handleSubmit}>
-          <div className="form-group">
-            <label htmlFor="username">Username</label>
-            <input id="username" type="text" className="form-control" />
-          </div>
-          <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <input id="password" type="text" className="form-control" />
-          </div>
+          <Input
+            name="username"
+            value={username}
+            label="Username"
+            onChange={this.handleChange}
+          />
+          <Input
+            name="password"
+            value={password}
+            label="Password"
+            onChange={this.handleChange}
+          />
         </form>
         <button className="btn btn-primary">Login</button>
       </div>
